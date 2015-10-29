61dd75e132cf40ad1ca21559763f67264aec39d5
Alvaro Mangual <amangual@live.com>
2015-10-29 14:20:03 +0000

    Checkpoint 30 Models complete

---
+++ db/migrate/20151029141413_create_posts.rb
@@ -0,0 +1,14 @@
+class CreatePosts < ActiveRecord::Migration
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps null: false
+    end
+  end
+end
+
+class Post < ActiveRecord::Base
+  has_many :comments
+end

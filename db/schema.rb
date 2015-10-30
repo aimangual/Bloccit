bb3ec274c2f011e6d806e93ec8aa074487dcda99
Alvaro Mangual <amangual@live.com>
2015-10-30 17:21:57 +0000

    Checkpoint 33 CRUD before Heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,37 +13,20 @@
 
 ActiveRecord::Schema.define(version: 20151029143059) do
 
-  create_table "answers", force: :cascade do |t|
+  create_table "comments", force: true do |t|
     t.text     "body"
     t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "answers", ["post_id"], name: "index_answers_on_post_id"
-
-  create_table "comments", force: :cascade do |t|
-    t.text     "body"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
-  create_table "posts", force: :cascade do |t|
+  create_table "posts", force: true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "questions", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.boolean  "resolved"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end

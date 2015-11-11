83b4d5a89e72fd47e8379c031bb0224282b912a4
Alvaro Mangual <amangual@live.com>
2015-11-11 21:05:07 +0000

    Assignment 42 Complete

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,3 +1,7 @@
 class Topic < ActiveRecord::Base
     has_many :posts
+    
+    validates :title, length: { minimum: 5 }, presence: true
+    validates :topic, presence: true
+    validates :user, presence: true
 end

371554750ce4da4a2dc93af2c4cd14e1b789841f
Alvaro I. Mangual <amangual@live.com>
2015-11-15 11:26:27 -0600

    Complete voting assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,6 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  # validates :topic, presence: true
-  # validates :user, presence: true
   
   mount_uploader :image, ImageUploader
 
@@ -28,7 +26,7 @@ class Post < ActiveRecord::Base
   end
 
   def update_rank
-    age = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
+    age = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
     new_rank = points + age
 
     update_attribute(:rank, new_rank)

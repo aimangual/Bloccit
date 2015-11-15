57c859d4b4127cabc1c9bebefbcb379188daf558
Alvaro I. Mangual <amangual@live.com>
2015-11-15 13:17:59 -0600

    Checkpoint 50 Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,10 +8,10 @@ class Post < ActiveRecord::Base
 
   validates :title, length: { minimum: 5 }, presence: true
   validates :body, length: { minimum: 20 }, presence: true
-  
-  mount_uploader :image, ImageUploader
+  validates :topic, presence: true
+  validates :user, presence: true
 
-  after_create :create_vote
+  mount_uploader :image, ImageUploader
 
   def up_votes
     votes.where(value: 1).count
@@ -26,13 +26,11 @@ class Post < ActiveRecord::Base
   end
 
   def update_rank
-    age = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24)
+    age = (created_at - Time.new(1970,1,1)) / (60 * 60 * 24) # 1 day in seconds
     new_rank = points + age
 
     update_attribute(:rank, new_rank)
   end
-
-  private
 
   def create_vote
     user.votes.create(value: 1, post: self)

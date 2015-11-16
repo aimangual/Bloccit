46ba8e4d1defffe51a235f9ec198fc56c50b23f6
Alvaro I. Mangual <amangual@live.com>
2015-11-16 10:13:23 -0600

    Checkpoint 51 complete

---
+++ app/mailers/application_mailer.rb
@@ -0,0 +1,4 @@
+class ApplicationMailer < ActionMailer::Base
+  default from: "amangual@live.com"
+  layout 'mailer'
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :votes, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   mount_uploader :avatar, AvatarUploader
 
@@ -16,5 +15,9 @@ class User < ActiveRecord::Base
 
   def moderator?
     role == 'moderator'
+  end
+
+  def favorited(post)
+    favorites.where(post_id: post.id).first
   end
 end

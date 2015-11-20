bb0c0983d2f189733434b1a3b037e03e39534ca5
Alvaro I. Mangual <amangual@live.com>
2015-11-19 19:03:50 -0600

    Checkpoint 51 favoriting errors in Mailer

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
-
+ 
 describe User do
-
+ 
   include TestFactories
-
+ 
   describe "#favorited(post)" do
-   xit "returns `nil` if the user has not favorited the post" do
-   end
-
-   xit "returns the appropriate favorite if it exists" do
-   end
- end
+    xit "returns `nil` if the user has not favorited the post" do
+    end
+ 
+    xit "returns the appropriate favorite if it exists" do
+    end
+  end
 end

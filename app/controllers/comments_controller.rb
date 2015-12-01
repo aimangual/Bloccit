10cfbd74b8594f16b7df55dad09998411821f507
Alvaro I. Mangual <amangual@live.com>
2015-12-01 16:16:30 -0600

    Checkpoint 57 Destroying with AJAX Updated

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,26 +1,31 @@
 class CommentsController < ApplicationController
-  
+  respond_to :html
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.new(comment_params)
-    @comment.user_id = current_user.id
+    @comment = current_user.comments.build(comment_params)
+    @comment.post = @post
 
+    authorize @comment
     if @comment.save
-      redirect_to [@post.topic, @post], notice: "Comment saved successfully."
+      flash[:notice] = "Comment was saved."
     else
-      redirect_to [@post.topic, @post], notice: "Comment failed to save."
+      flash[:error] = "There was an error saving the comment. Please try again."
+    end
+    
+    respond_with(@comment) do |format|
+      format.html { redirect_to [@post.topic, @post] }
     end
   end
 
   def destroy
     @post = Post.find(params[:post_id])
     @comment = @post.comments.find(params[:id])
-    authorize @comment
 
+    authorize @comment
     if @comment.destroy
-      flash[:notice] = "Comment was removed."
+      flash[:notice] = "Comment was removed"
     else
-      flash[:error] = "Comment couldn't be deleted. Try again."
+      flash[:error] = "Comment could not be deleted. Try again."
     end
 
     respond_to do |format|
@@ -29,10 +34,7 @@ class CommentsController < ApplicationController
     end
   end
 
-  private
-
   def comment_params
     params.require(:comment).permit(:body)
   end
-
 end

d63825c281bce652e0f4513a16c021fcd202d5b8
Alvaro I. Mangual <amangual@live.com>
2015-12-01 15:57:41 -0600

    Assignment 56 Testing Users Complete

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,60 +1,18 @@
-require 'rails_helper'
+FactoryGirl.define do
+  
+  factory :user do
+    name "Joe Bob"
+    sequence(:email, 100) { |n| "person#{n}@example.com"}
+    password "helloworld"
+    password_confirmation "helloworld"
+    confirmed_at Time.now
 
-describe User do 
-
-  include TestFactories
-
-  describe "#favorited(post)" do
-    before do
-      @post = associated_post
-      @user = authenticated_user
-    end
-
-    it "returns 'nil' if the user has not favorited the post" do
-      expect(@user.favorited(@post)).to be_nil
-    end
-
-    it "returns the appropriate favorite if it exists" do
-      # create a new favorite with the post/user from test factories
-      favorite = Favorite.create(post: @post, user: @user)
-
-      expect(@user.favorited(@post)).to eq(favorite)
-    end  
-
-    it "returns nil if the user has favorited a different post than the post being passed into 'favorited'" do
-      # create a new post and a favorite
-      @post2 = associated_post(title: "new title")
-      favorite = Favorite.create(post: @post2, user: @user)
-
-      # expecting favorited to return nil when passed in the first post
-      expect(@user.favorited(@post)).to be_nil
+    factory :user_with_post_and_comment do
+      after(:build) do |user|
+        post = create(:post, user: user)
+        create(:comment, user: user, post: post)
+      end
     end
   end
 
-  describe ".top_rated" do
-
-    before do
-      @user1 = create(:user)
-      post = create(:post, user: @user1)
-      create(:comment, user: @user1, post: post)
-
-      @user2 = create(:user)
-      post = create(:post, user: @user2)
-      2.times { create(:comment, user: @user2, post: post)}
-    end
-
-    it "returns users ordered by comments + posts" do
-      expect( User.top_rated).to eq([@user2, @user1])
-    end
-
-    it "stores a 'posts_count' on user" do
-      users = User.top_rated
-      expect( users.first.posts_count).to eq(1)
-    end
-
-    it "stores a 'comments_count' on user" do
-      users = User.top_rated
-      expect(users.first.comments_count).to eq(2)
-    end
-  end
 end
--- spec/features/profiles_spec.rb
+++ spec/features/profiles_spec.rb
@@ -3,18 +3,17 @@ require 'rails_helper'
 describe "Visiting profiles" do
 
   include TestFactories
-  include Warden::Test::Helpers
-  Warden.test_mode!
 
-  before do 
+  before do
     @user = authenticated_user
-    @topic = Topic.create(description: "A topic")
-    @post = post_without_user(user: @user, topic: @topic)
-    @comment = comment_without_email(post: @post, user: @user)
+    @topic = Topic.create(name: "A topic", description: "A topic")
+    @post = associated_post(user: @user, topic: @topic)
+    @comment = Comment.new(user: @user, post: @post, body: "A Comment")
+    allow(@comment).to receive(:send_favorite_emails)
+    @comment.save!
   end
 
   describe "not signed in" do
-
     it "shows profile" do
       visit user_path(@user)
       expect(current_path).to eq(user_path(@user))
@@ -23,21 +22,6 @@ describe "Visiting profiles" do
       expect( page ).to have_content(@post.title)
       expect( page ).to have_content(@comment.body)
     end
+
   end
-
-  describe "signed in as admin" do
-
-    before { login_as authenticated_user(role: 'admin'), scope: :user }
-
-    it "shows profile" do
-      visit user_path(@user)
-      expect(current_path).to eq(user_path(@user))
-
-      expect( page ).to have_content(@user.name)
-      expect( page ).to have_content(@post.title)
-      expect( page ).to have_content(@comment.body)
-    end
-  end
-
-
 end

d50624ff811063010895de1242383818effed575
Alvaro I. Mangual <amangual@live.com>
2015-12-04 14:18:40 -0600

    User spec updated

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -34,7 +34,7 @@ describe User do
   describe ".top_rated" do
 
     before do
-      @user1 = create(:user)
+      @user1 = create(:user_with_post_and_comment)
       post = create(:post, user: @user1)
       create(:comment, user: @user1, post: post)
 
